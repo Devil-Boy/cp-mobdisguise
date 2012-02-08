cc434384703a34e043745215b6107ecc46e8cecc
Devil boy <kws512@hotmail.com>
2012-02-07 18:10:07 -0800

    Updated for MB v1.7 API

--- src/pgDev/bukkit/CPMD/MDEventListener.java
+++ src/pgDev/bukkit/CPMD/MDEventListener.java
@@ -14,49 +14,49 @@ public class MDEventListener implements Listener {
 	
 	// Listen Methods
 	@EventHandler
-	public void onDisguiseAsMob(DisguiseAsMobEvent event) {
-		if (!plugin.hasPermissions(event.getPlayer(), "CPMD.disguise.free")) {
-			if (plugin.cpAPI.hasAccount(event.getPlayer().getName(), plugin)) {
-				if (plugin.cpAPI.hasPoints(event.getPlayer().getName(), plugin.pluginSettings.disguiseCosts.get(event.getMobType()), plugin)) {
-					plugin.cpAPI.removePoints(event.getPlayer().getName(), plugin.pluginSettings.disguiseCosts.get(event.getMobType()), "Disguised as a " + event.getMobType(), plugin);
-					int remainingPoints = plugin.cpAPI.getPoints(event.getPlayer().getName(), plugin);
-					if (remainingPoints == 1) {
-						event.getPlayer().sendMessage(ChatColor.BLUE + "You have " + remainingPoints + " point remaining.");
+	public void onDisguise(DisguiseEvent ev) {
+		if (ev instanceof DisguiseAsMobEvent) {
+			DisguiseAsMobEvent event = (DisguiseAsMobEvent) ev;
+			if (!plugin.hasPermissions(event.getPlayer(), "CPMD.disguise.free")) {
+				if (plugin.cpAPI.hasAccount(event.getPlayer().getName(), plugin)) {
+					if (plugin.cpAPI.hasPoints(event.getPlayer().getName(), plugin.pluginSettings.disguiseCosts.get(event.getMobType()), plugin)) {
+						plugin.cpAPI.removePoints(event.getPlayer().getName(), plugin.pluginSettings.disguiseCosts.get(event.getMobType()), "Disguised as a " + event.getMobType(), plugin);
+						int remainingPoints = plugin.cpAPI.getPoints(event.getPlayer().getName(), plugin);
+						if (remainingPoints == 1) {
+							event.getPlayer().sendMessage(ChatColor.BLUE + "You have " + remainingPoints + " point remaining.");
+						} else {
+							event.getPlayer().sendMessage(ChatColor.BLUE + "You have " + remainingPoints + " points remaining.");
+						}
 					} else {
-						event.getPlayer().sendMessage(ChatColor.BLUE + "You have " + remainingPoints + " points remaining.");
+						event.getPlayer().sendMessage(ChatColor.RED + "You do not have the required commandpoints to disguise as this mob.");
+						event.setCancelled(true);
 					}
 				} else {
-					event.getPlayer().sendMessage(ChatColor.RED + "You do not have the required commandpoints to disguise as this mob.");
+					event.getPlayer().sendMessage(ChatColor.RED + "You do not have a commandpoints account.");
 					event.setCancelled(true);
 				}
-			} else {
-				event.getPlayer().sendMessage(ChatColor.RED + "You do not have a commandpoints account.");
-				event.setCancelled(true);
+			}
+		} else if (ev instanceof DisguiseAsPlayerEvent) {
+			DisguiseAsPlayerEvent event = (DisguiseAsPlayerEvent) ev;
+			if (!plugin.hasPermissions(event.getPlayer(), "CPMD.disguise.free")) {
+				if (plugin.cpAPI.hasAccount(event.getPlayer().getName(), plugin)) {
+					if (plugin.cpAPI.hasPoints(event.getPlayer().getName(), plugin.pluginSettings.disguiseCosts.get("player"), plugin)) {
+						plugin.cpAPI.removePoints(event.getPlayer().getName(), plugin.pluginSettings.disguiseCosts.get("player"), "Disguised as a player:" + event.getName(), plugin);
+						int remainingPoints = plugin.cpAPI.getPoints(event.getPlayer().getName(), plugin);
+						if (remainingPoints == 1) {
+							event.getPlayer().sendMessage(ChatColor.BLUE + "You have " + remainingPoints + " point remaining.");
+						} else {
+							event.getPlayer().sendMessage(ChatColor.BLUE + "You have " + remainingPoints + " points remaining.");
+						}
+					} else {
+						event.getPlayer().sendMessage(ChatColor.RED + "You do not have the required commandpoints to disguise as another person.");
+						event.setCancelled(true);
+					}
+				} else {
+					event.getPlayer().sendMessage(ChatColor.RED + "You do not have a commandpoints account.");
+					event.setCancelled(true);
+				}
 			}
 		}
 	}
-	
-	@EventHandler
-	public void onDisguiseAsPlayer(DisguiseAsPlayerEvent event) {
-		if (!plugin.hasPermissions(event.getPlayer(), "CPMD.disguise.free")) {
-			if (plugin.cpAPI.hasAccount(event.getPlayer().getName(), plugin)) {
-				if (plugin.cpAPI.hasPoints(event.getPlayer().getName(), plugin.pluginSettings.disguiseCosts.get("player"), plugin)) {
-					plugin.cpAPI.removePoints(event.getPlayer().getName(), plugin.pluginSettings.disguiseCosts.get("player"), "Disguised as a player:" + event.getName(), plugin);
-					int remainingPoints = plugin.cpAPI.getPoints(event.getPlayer().getName(), plugin);
-					if (remainingPoints == 1) {
-						event.getPlayer().sendMessage(ChatColor.BLUE + "You have " + remainingPoints + " point remaining.");
-					} else {
-						event.getPlayer().sendMessage(ChatColor.BLUE + "You have " + remainingPoints + " points remaining.");
-					}
-				} else {
-					event.getPlayer().sendMessage(ChatColor.RED + "You do not have the required commandpoints to disguise as another person.");
-					event.setCancelled(true);
-				}
-			} else {
-				event.getPlayer().sendMessage(ChatColor.RED + "You do not have a commandpoints account.");
-				event.setCancelled(true);
-			}
-		}
-	}
-	
 }
